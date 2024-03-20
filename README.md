b906256d52000d64f8f69c6c13d3584de8061483
ilhamygp <ilhamyogha@gmail.com>
2024-03-20 14:02:28 +0700

    Release version 1.0.0

---
+++ README.md
@@ -0,0 +1,9 @@
+## Required Tool
+- Node
+
+## How to run
+1. Open terminal
+2. Clone this repo
+3. `npm install`
+4. `node index.js`
+5. Customize index.js what you need and re-run step 4
---
+++ index.js
@@ -0,0 +1,22 @@
+function sum(a, b) {
+  return a + b
+}
+
+function sub(a, b) {
+  return a - b
+}
+
+function mult(a, b) {
+  return a * b
+}
+
+function div(a, b) {
+  return a / b
+}
+
+module.exports = {
+  sum,
+  sub,
+  mult,
+  div
+}
